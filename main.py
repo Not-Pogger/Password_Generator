2994bfc46ceb43779862b6e7c8a468e201c2167b
NotPogger <81515451+not-pogger@users.noreply.github.com>
2022-08-02 01:28:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def generate_random_password():
+	length = 10
+
+	random.shuffle(characters)
+	
+	password = []
+	for i in range(length):
+		password.append(random.choice(characters))
+
+	random.shuffle(password)
+
+	print("".join(password))
+
+
+
+generate_random_password()
